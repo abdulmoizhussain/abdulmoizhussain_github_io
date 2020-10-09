c84a37dd7f095e82c9147bb65dc2e101ee2f54d6
Abdul Moiz <moizhus123@gmail.com>
2020-10-10 00:47:04 +0500

    new one

--- deploy.bat
+++ deploy.bat
@@ -12,10 +12,12 @@ REM enter user input
 REM https://stackoverflow.com/questions/1223721/in-windows-cmd-how-do-i-prompt-for-user-input-and-use-the-result-in-another-com
 set /p commit_message="Enter Commit Message: "
 
-git commit -m %commit_message%
+echo.
+
+git commit -m "%commit_message%"
 git push
 
 cmd /k cd ..
 
-REM below line is optional but might be necessary
+REM below line is optional if you want to check the changes. but it will be necessary.
 REM cmd /k rmdir /s /q .\abdulmoizhussaingithubio
