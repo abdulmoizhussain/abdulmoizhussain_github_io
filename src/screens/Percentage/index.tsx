6b20b500cb1721968c949c5b0ffcaa35c7f7cfdc
Abdul Moiz <moizhus123@gmail.com>
2021-04-03 19:28:00 +0500

    applied bignumber.js library.

--- src/screens/Percentage/index.tsx
+++ src/screens/Percentage/index.tsx
@@ -1,57 +1,71 @@
 import React from 'react';
-
-
+import BigNumber from "bignumber.js";
 
 class Percentage extends React.Component {
-    state = {
-        dollarRate: "0",
-        price: "0",
-        bankFeePercent: "5",
-        resultNormal: 0,
-        resultNormalFeePercent: 0,
-        resultNormalAfterAddingBankFee: 0,
-        actualPriceCut: 2123.78,
-    };
+  state = {
+    dollarRate: "154.97",
+    price: "12.99",
+    bankFeePercent: "5",
+    resultNormal: 0,
+    resultNormalFeePercent: 0,
+    resultNormalAfterAddingBankFee: 0,
+    actualPriceCut: 2123.78,
+    percentageOfActualPrice: "0",
+    serviceFee: "0",
+  };
 
-    calculateNormalResult = () => {
-        const { price, dollarRate, bankFeePercent } = this.state;
-        const bankFeePercentInt = parseFloat(bankFeePercent);
-        const resultNorm = parseFloat(price) * parseFloat(dollarRate);
+  componentDidMount() {
+    this.calculateNormalResult();
+  }
 
-        this.setState({
-            resultNormal: resultNorm,
-            resultNormalFeePercent: (resultNorm / 100) * bankFeePercentInt,
-            resultNormalAfterAddingBankFee: ((resultNorm / 100) * bankFeePercentInt) + resultNorm,
-        });
-    };
+  calculateNormalResult = () => {
+    const { price, dollarRate, bankFeePercent, actualPriceCut } = this.state;
+    const bankFeePercentBig = new BigNumber(bankFeePercent);
 
-    render() {
-        const { price, dollarRate, bankFeePercent, resultNormal, resultNormalFeePercent, resultNormalAfterAddingBankFee, actualPriceCut } = this.state;
-        return <div>
-            <div>
-                Rate:&nbsp;
+    const resultNormBig = new BigNumber(price).multipliedBy(dollarRate);
+
+    const resultNormalFeePercent = resultNormBig.dividedBy(100).multipliedBy(bankFeePercentBig);
+    const percentageOfActualPrice = new BigNumber(actualPriceCut).dividedBy(resultNormBig).multipliedBy(100);
+
+    this.setState({
+      resultNormal: resultNormBig.toFixed(2),
+      resultNormalFeePercent: resultNormalFeePercent.toFixed(2),
+      resultNormalAfterAddingBankFee: resultNormalFeePercent.plus(resultNormBig).toFixed(2),
+      percentageOfActualPrice: percentageOfActualPrice.toFixed(2),
+      serviceFee: percentageOfActualPrice.minus(100).toFixed(2),
+    });
+  };
+
+  render() {
+    const { price, dollarRate, bankFeePercent, resultNormal, resultNormalFeePercent, resultNormalAfterAddingBankFee, actualPriceCut, percentageOfActualPrice, serviceFee } = this.state;
+    return <div>
+      <div>
+        Rate:&nbsp;
             <input type="text" placeholder="Dollar Rate" value={dollarRate} onChange={ev => {
-                    this.setState({ dollarRate: ev.target.value }, this.calculateNormalResult);
-                }} />
-            </div>
-            <div>
-                Price:&nbsp;
+          this.setState({ dollarRate: ev.target.value }, this.calculateNormalResult);
+        }} />
+      </div>
+      <div>
+        Price:&nbsp;
             <input type="text" placeholder="Price" value={price} onChange={ev => {
-                    this.setState({ price: ev.target.value }, this.calculateNormalResult);
-                }} />
-            </div>
-            <div>
-                Bank Fee %:&nbsp;
+          this.setState({ price: ev.target.value }, this.calculateNormalResult);
+        }} />
+      </div>
+      <div>
+        Bank Fee %:&nbsp;
             <input type="text" placeholder="Bank Fee %" value={bankFeePercent} onChange={ev => {
-                    this.setState({ bankFeePercent: ev.target.value }, this.calculateNormalResult);
-                }} />
-            </div>
-            <div>{resultNormal}</div>
-            <div>{bankFeePercent}% {resultNormalFeePercent}</div>
-            <div>With {bankFeePercent}% {resultNormalAfterAddingBankFee}</div>
-            <div>{resultNormal} is {((resultNormal / actualPriceCut) * 100).toFixed(2)}% of {actualPriceCut} {"-->"} service fee is {(100 - ((resultNormal / actualPriceCut) * 100)).toFixed(2)}</div>
-        </div>;
-    }
+          this.setState({ bankFeePercent: ev.target.value }, this.calculateNormalResult);
+        }} />
+      </div>
+      <hr />
+      <div>{resultNormal}</div>
+      <hr />
+      <div>{bankFeePercent}% {resultNormalFeePercent}</div>
+      <div>With {bankFeePercent}% {resultNormalAfterAddingBankFee}</div>
+      <hr />
+      <div>{actualPriceCut} is {percentageOfActualPrice}% of {resultNormal} {"-->"} service fee is {serviceFee}%</div>
+    </div>;
+  }
 }
 
 export default Percentage;
